b009cc3da027991649185278791467fa28b72902
Yateo <olivier@yateo.com>
2018-10-29 15:24:04 +0100

    fix autocomplete order adress

--- override/modules/autogoogleplace/autogoogleplace.php
+++ override/modules/autogoogleplace/autogoogleplace.php
@@ -10,15 +10,15 @@ class AutoGooglePlaceOverride extends AutoGooglePlace
         {
             $page_name = $this->getPageName();
 
-            if ($page_name == 'order' && !Tools::getIsset('newAddress'))
-            	return;
-
             $req = 'SELECT page FROM `'._DB_PREFIX_.'meta` WHERE `id_meta` IN ( '.pSQL($metas).') AND page="'.pSQL($page_name).'"';
             $sql = Db::getInstance()->getRow($req);
 
             if (!empty($sql)) 
             {
                 $this->context->controller->addJS(_MODULE_DIR_.$this->name.'/views/js/'.$this->name.'.js');
+
+                // if ($page_name == 'order' && !Tools::getIsset('newAddress'))
+                //     return;
                 
                 if( (bool) Configuration::get('AUTOGOOGLEPLACE_FORCE_15') == true || version_compare(_PS_VERSION_, "1.6.0.2", "<"))
                     echo "<script type='text/javascript'>var mapsapikey = '".Configuration::get('AUTOGOOGLEPLACE_KEY')."';</script>";
