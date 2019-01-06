0ef5057ede3e29fc727fcf4f8c0c717f5c3dd78f
root <root@dev.dev.tiboott.com>
2019-01-06 19:18:41 +0100

    fix ajax orderfees for refresh cart

--- modules/orderfees/controllers/front/ajax.php
+++ modules/orderfees/controllers/front/ajax.php
@@ -14,7 +14,7 @@ class orderfeesAjaxModuleFrontController extends ModuleFrontController
 
 	public function updateFees()
     {
-    	$this->context->cart->current_type = $this->module::DISABLE_CHECKING;
+    	$this->context->cart->current_type = OrderFees::DISABLE_CHECKING;
         
         CartRule::autoRemoveFromCart();
         CartRule::autoAddToCart();
@@ -28,26 +28,7 @@ class orderfeesAjaxModuleFrontController extends ModuleFrontController
         {
             if (!$discount['is_fee']) {
                 continue;
-            }
-
-            $add_price = 0;
-            if (preg_match('@primeur@i', $discount['name']))
-            {
-	            foreach ($products as $product)
-	            {
-	            	if ($product['wine'])
-	            	{
-	            		$add_price += ($product['total_wt']*1.5)/100;
-	            	}
-	            }
-                $discounts[$index]['obj']->reduction_amount += $add_price;
-                // $discount['obj']->unit_value_real += $add_price;
-                // $discount['obj']->unit_value_real_exc += $add_price;
-                $discounts[$index]['reduction_amount'] += $add_price;
-                $discounts[$index]['value_real'] -= $add_price;
-                $discounts[$index]['value_tax_exc'] -= $add_price;
-            }
-            
+            }            
 
             if ($discount['is_fee']) {
                 $option_selected = isset($cookie->{'enable_option_' . $discount['id_cart_rule']});
@@ -67,9 +48,6 @@ class orderfeesAjaxModuleFrontController extends ModuleFrontController
                 unset($discounts[$index]);
             }
         }
-        // echo '<pre>';
-        // print_r($this->context->cart);
-        // die();
         
         $this->context->smarty->assign(array(
             'fees' => $fees,
